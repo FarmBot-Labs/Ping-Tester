af57304432398b3da6cbd8e6f08ce75785c90f78
Rick Carlino <rick.carlino@gmail.com>
2019-05-28 12:57:55 -0500

    Done

---
+++ src/README.md
@@ -0,0 +1,16 @@
+# Ping Tester
+
+This is an example application that helps developers ping their bots remotely.
+
+It's also a good example of how to write non-Farmware extensions.
+
+# Installation
+
+0. Clone repo
+0. `npm install`
+
+#  Run
+
+0. `npm  start`
+
+Open `http://localhost:1235/` and enter a  device token.
--- src/active.tsx
+++ src/active.tsx
@@ -9,32 +9,51 @@ interface Props {
 interface State {
   total: number;
   totalOk: number;
+  inProgress: boolean;
 }
 
 export class Active extends Component<Props, State> {
   state: State = {
     total: 0,
-    totalOk: 0
+    totalOk: 0,
+    inProgress: false
   };
 
   get percentage() {
-    return this.state.totalOk / this.state.total;
+    return (((this.state.totalOk / this.state.total) || 0) * 100).toFixed(2);
   }
 
   ping = () => {
-    this.setState({ total: this.state.total + 1 });
-    const no = () => { };
-    const ok = () => this.setState({ totalOk: this.state.totalOk + 1 });
+    this.setState({ total: this.state.total + 1, inProgress: true });
+    const no = () => this.setState({ inProgress: false });
+    const ok = () => this.setState({
+      totalOk: this.state.totalOk + 1,
+      inProgress: false
+    });
 
     this.props.farmbot.ping().then(ok, no);
   };
 
+  statusDisplay = () => {
+    if (this.state.inProgress) {
+      return "Pinging...";
+    } else {
+      return [
+        this.state.totalOk,
+        "/",
+        this.state.total,
+        "=",
+        (this.percentage || 0),
+        "% Success Rate"
+      ].join(" ");
+    }
+  }
   render({ }, { }) {
     return <div>
       <button onClick={this.props.onExit}>Try Different Token</button>
       <button onClick={this.ping}>Perform Ping</button>
       <br />
-      {this.state.totalOk}/{this.state.total} = {this.percentage || 0}% Success Rate
+      {this.statusDisplay()}
     </div>;
   }
 }
